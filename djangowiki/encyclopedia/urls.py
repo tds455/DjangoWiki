94a9f936d5e4e8d59ba4506dcf17a0d1af170b0c
tds455 <tds-1993@outlook.com>
2022-02-23 16:32:53 +0900

    Added view prototype for wiki pages.  Added page.html
    The display function in views.py will take a request to display a page, validate it, and either return an error message or the requested page, passing values into the page.html template
    
    TODO: Convert text from markdown to html and added an error page html template to complete the function.

--- djangowiki/encyclopedia/urls.py
+++ djangowiki/encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # Path to show index page
+    path("", views.index, name="index"),
+    # Path to show a certain page
+    path("<str:title>", views.display, name="display")
 ]
--- djangowiki/encyclopedia/views.py
+++ djangowiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +9,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# Display the requested page
+# ref: https://docs.djangoproject.com/en/4.0/intro/tutorial03/
+def display(request, title):
+    # Check if page exists / validate input
+    if not util.get_entry(title):
+        # Placeholder return value.  
+        return HttpResponse("This is a placeholder error")
+    #Return page
+    else:
+        page = util.get_entry(title)
+        # provide context
+
+        #Convert content from markdown to html
+
+        #Pass html into render function.
+        return render(request, 'encyclopedia/page.html', {"title": title, "pagebody": page})
