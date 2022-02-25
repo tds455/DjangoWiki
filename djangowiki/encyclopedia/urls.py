cae100708b19d7232813d5457e2fb4ad3c8b0e29
tds455 <tds-1993@outlook.com>
2022-02-25 15:15:01 +0900

    Updated comments

--- djangowiki/encyclopedia/urls.py
+++ djangowiki/encyclopedia/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     # Path to show index page
     path('', views.index, name='index'),
     # Path to show a certain page
-    # <str:title> allows the GET request to match to a path
     path('wiki/<str:title>', views.display, name='display'),
     # Path to process search input
     path('search', views.search, name='search'),
